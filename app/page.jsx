f1a0d0cf439147afcd91d1b09df8e6b485a5d2a0
Aikon <aikonidisa@gmail.com>
2025-04-08 01:08:01 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -57,4 +57,5 @@ export default function Home() {
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-        className
+className="max-w-2xl mx-auto"
+>
