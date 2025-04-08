7002fb400e5aaa1697099c236d50819508583d20
Aikon <aikonidisa@gmail.com>
2025-04-08 15:11:49 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<body className="bg-red-500 text-white p-10">Hello World {children}</body>
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
